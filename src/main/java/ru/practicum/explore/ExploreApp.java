2722fada467ecdda7a1cc0502ca46fbefaa016c7
ulivaa <ivanova788@mail.ru>
2022-09-14 01:55:00 +0300

    init commit

---
+++ src/main/java/ru/practicum/explore/ExploreApp.java
@@ -0,0 +1,13 @@
+package ru.practicum.explore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
+public class ExploreApp {
+    public static void main(String[] args) {
+        SpringApplication.run(ExploreApp.class, args);
+    }
+
+}
